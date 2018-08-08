6c8bb0b58823758355d3a63215ce332cfe3a33b1
Matt Childers <matthew.childers1992@gmail.com>
2018-08-07 18:47:51 -0600

    100 orders each way

--- src/core/LiveOrderbook.ts
+++ src/core/LiveOrderbook.ts
@@ -248,8 +248,8 @@ export class LiveOrderbook extends Duplex implements Orderbook {
     }
 
     private processSnapshot(snapshot: SnapshotMessage) {
-        snapshot.asks = snapshot.asks.slice(0, 25);
-        snapshot.bids = snapshot.bids.slice(0, 25);
+        snapshot.asks = snapshot.asks.slice(0, 100);
+        snapshot.bids = snapshot.bids.slice(0, 100);
         this._book.fromState(snapshot);
         this._sourceSequence = snapshot.sourceSequence;
         this.snapshotReceived = true;
